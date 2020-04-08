5aecf0ab01e2533ecce61ecae05bdf2f44004098
Grinnbob <grifon12358@gmail.com>
2020-04-08 03:05:03 +0300

    deleted

--- crawlers/linkedin/scraper.js
+++ crawlers/linkedin/scraper.js
@@ -31,34 +31,3 @@ async function playTest(url) {
   await playTest("https://www.linkedin.com/uas/login?fromSignIn=true&trk=cold_join_sign_in");
   process.exit(1);
 })();
-
-
-let username = process.argv[2];
-
-let scrape = async () => {
-    const browser = await puppeteer.launch({headless: false});
-    const page = await browser.newPage();
-    await page.goto('https://www.linkedin.com/in/'+ username +'/');
-    await page.waitFor(1000);
-    await page.screenshot({path: 'linkedin.png'});
-    const result = await page.evaluate(() => {
-
-        let name = document.querySelector('#name').innerText;
-        let title = document.querySelector('.headline').innerText;
-        let summary = document.querySelector('.headline').childNodes[0].innerText;
-
-        return {
-            "name": name,
-            "title": title,
-            "summary": summary
-        }
-
-    });
-
-    browser.close();
-    return result;
-};
-
-scrape().then((value) => {
-    console.log(value);
-});
