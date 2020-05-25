65ac7607e93c32976174e380223ce856ec0a5618
Grinnbob <grifon12358@gmail.com>
2020-05-25 15:03:19 +0300

    403 error in axios catched

--- frontend/src/api/axios-auth.js
+++ frontend/src/api/axios-auth.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-
+import store from '../../store/index'
 
 const instance = axios.create({
     baseURL: process.env.VUE_APP_API_URL,
@@ -11,5 +11,31 @@ const instance = axios.create({
         }
     }
 });
+
+instance.interceptors.response.use(function (response) {
+    // Do something with response data
+    return response;
+  }, function (error) {
+    // Do something with response error
+    let msg = error.toString();
+    if(msg.includes('403')){
+        //console.log('....', localStorage.getItem('token'))
+
+        store.dispatch("auth/logout").then(
+            resolve => {
+                //_this.$router.push("login");
+              },
+              reject => {
+                console.log("error here: ", reject);
+              }
+            )
+            .catch(err => {
+              console.error("login error: ", err);
+            });
+        //router.push("login");
+        console.log(`Error: 403 here`);
+    }
+    return Promise.reject(error);
+  });
 
 export default instance
