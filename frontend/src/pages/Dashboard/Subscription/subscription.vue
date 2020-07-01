8989f6474ede80e1b9e85dd00edcea9342451e64
kshilov <ks.shilov@gmail.com>
2020-07-01 19:10:36 +0300

    Added subscription and website chat

--- frontend/src/pages/Dashboard/Subscription/subscription.vue
+++ frontend/src/pages/Dashboard/Subscription/subscription.vue
@@ -1,41 +1,85 @@
 <template>
-    <div>
+    <div>      
       <card>
-        <div class="col-4 d-flex align-self-center">
-          <h3>
-            <i class="nc-icon nc-circle-09"></i> Subscription settings
-          </h3>
-        </div>
-        <card>
-            <div id="paddle">
-                <a @click.prevent="agencyPlanActivate()" href="#">Activate Agency plan</a>
-                <a @click.prevent="solopreneurPlanActivate()" href="#">Activate Solopreneur plan</a>
-
+        <div class="row">
+            <div class="col-4 d-flex align-self-center">
+                <h3>
+                <i class="nc-icon nc-single-copy-04"></i> Subscription settings
+                </h3>
             </div>
+            <div class="col-8 d-flex flex-row-reverse align-self-center">
+            </div>
+        </div>
         </card>
-      </card>
-  
-      <modals-container/>
+        
+        <card>
+                <div class="col-12">
+                  <el-table
+                  stripe
+                  style="width: 100%;"
+                  :data="tableData"
+                  max-height="500"
+                  border>
+                  <el-table-column
+                    prop="plan"
+                    label="Subscription plan"
+                    width="180">
+                  </el-table-column>
+                  <el-table-column
+                    prop="price"
+                    label="Price"
+                    width="180">
+                  </el-table-column>
+                 
+                  <el-table-column
+                    prop="link"
+                    label=""
+                    width="180">
+                    <template slot-scope="scope">
+                      <a @click.prevent="activateSubscription(scope.row)" href="#">Activate</a>
+                  </template>
+                  </el-table-column>
+                </el-table>
+                </div>
+        </card>   
+
+
     </div>
   
   </template>
   <script>
   import axios from "@/api/axios-auth";
-  import { Notification, Select, Option } from "element-ui";
+  import { Table, TableColumn } from "element-ui";
     
 
   export default {
+    components: {
+      [Table.name]: Table,
+      [TableColumn.name]: TableColumn
+    },
     data() {
       return {
-        user_data: {}
+        tableData: [{
+          plan: 'Solopreneur',
+          price: '$19/month',
+          link: 'solo',
+        }, {
+          plan: 'Agency',
+          price: '$49/month',
+          link: 'agency',
+        }]
+      
       };
     },
     methods: {
-        agencyPlanActivate(){
+        activateSubscription(data){
+          var plan = data.link;
+
+          if (plan == 'solo'){
+            Paddle.Checkout.open({ product: 599188 });
+          }else{
             Paddle.Checkout.open({ product: 599189 });
-        },
-        solopreneurPlanActivate(){
-          Paddle.Checkout.open({ product: 599188 });
+          }
         }
     },
     async mounted() {
